def6b9bd43395d1bfa1d084a0fda54333e796af1
kow sakazaki <kow@prominence.tv>
2014-05-14 23:51:56 +0900

    enchant only version3

--- index.php
+++ index.php
@@ -10,15 +10,9 @@ $ini = parse_ini_file("lib/config.ini", true);
     <meta name="apple-mobile-web-app-capable" content="yes">
 	<script type="text/javascript" src="usrobject/environ.js"></script>
 <?php
-    if ($library == "tmlib") {
-        echo '<script type="text/javascript" src="extlib/tmlib.min.0.3.0.js"></script>';
-        echo '<script type="text/javascript" src="sysobject/enforce.core.tmlib.js"></script>';
-        echo '<script type="text/javascript">_setTmlib();</script>';
-    } else if ($library == "enchant") {
-        echo '<script type="text/javascript" src="extlib/enchant.0.8.0-enforce.js"></script>';
-        echo '<script type="text/javascript" src="sysobject/enforce.core.enchant.js"></script>';
-        echo '<script type="text/javascript">_setEnchant();</script>';
-    }
+    echo '<script type="text/javascript" src="extlib/enchant.0.8.0-enforce.js"></script>';
+    echo '<script type="text/javascript" src="sysobject/enforce.core.enchant.js"></script>';
+    echo '<script type="text/javascript">_setEnchant();</script>';
 ?>
 <?php
     // #################################################################################
@@ -53,13 +47,7 @@ $ini = parse_ini_file("lib/config.ini", true);
     </style>
 </head>
 <?php
-    if ($library == "tmlib") {
-        echo '<body bgcolor="black">';
-        echo '<canvas id="stage"></canvas>';
-        echo '</body>';
-    } else {
-        echo '<body bgcolor="black">';
-        echo '</body>';
-    }
+    echo '<body bgcolor="black">';
+    echo '</body>';
 ?>
 </html>
